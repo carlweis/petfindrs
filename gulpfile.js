0970883802bdd7d73f16810ffe974382ea86684a
Carl Weis <carl@carlweis.com>
2016-01-10 00:04:53 -0600

    Setup gulp phpunit testing and install composer packages

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+
+var gulp    = require('gulp');
+var phpunit = require('gulp-phpunit');
+
+gulp.task('phpunit', function() {
+    var options = {debug: false};
+    gulp.src('phpunit.xml')
+        .pipe(phpunit('./vendor/bin/phpunit',options));
+});
+
+gulp.task('default', function(){
+    gulp.run('phpunit');
+    gulp.watch('app/**/*.php', function(){
+        gulp.run('phpunit');
+    });
+});
